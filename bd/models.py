37bf4e375d7e5a19caa711d85cb5df1a98fdfd59
Diego Larenas <111461360+larenas-d@users.noreply.github.com>
2023-02-01 21:21:56 -0300

    :sparkles: added model to work with page_home database

--- bd/models.py
+++ bd/models.py
@@ -1,31 +1,57 @@
 from datetime import datetime
 from sqlalchemy import Column, Integer, DateTime, Text, ForeignKey, Boolean
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 from .declarative_base import Base
+from uuid import uuid4
 
 
-class ReportError(Base):
-    """Model of register of error."""
-    __tablename__ = "report_error"
-
-    id = Column("id", Integer(), primary_key=True)
-    status = Column("status", Integer(), nullable=False)
-    message = Column("message", Text, nullable=False)
-    created_at = Column("created_at", DateTime(), default=datetime.now())
-    url_id = Column("url_id", Integer, ForeignKey("url.id", ondelete="CASCADE"), nullable=False)
-    is_notification = Column("is_notification", Boolean(), default=False, nullable=False)
-
-    def __str__(self):  # noqa
-        return str(self.id)
-
-
-class Url(Base):
-    """Model register url."""
-    __tablename__ = "url"
+class MessageType(Base):
+    """Alert."""
+    __tablename__ = "logs_page_messagetype"
 
     id = Column("id", Integer(), primary_key=True)
-    url = Column("url", Text, unique=True)
-    reports = relationship("ReportError", lazy='dynamic', cascade="all, delete, delete-orphan")
+    email = Column("email", Text, nullable=False)
+    created = Column("created", DateTime(), default=datetime.now())
+    modified = Column("modified", DateTime(), default=datetime.now())
+    uuid = Column("uuid", UUID(as_uuid=True), default=uuid4)
+    type_alert = Column("type_alert", Integer(), default=1)
+    sites = relationship("SiteAlert", back_populates="message_type")
 
     def __str__(self):  # noqa
-        return self.url
+        return str(self.email)
+
+
+class SiteAlert(Base):
+    __tablename__ = "logs_page_sitealert"
+
+    id = Column("id", Integer(), primary_key=True)
+    uuid = Column("uuid", UUID(as_uuid=True), default=uuid4)
+    created = Column("created", DateTime(), default=datetime.now())
+    modified = Column("modified", DateTime(), default=datetime.now())
+    url_site = Column("url_site", Text, nullable=False)
+    enable = Column("enable", Boolean, default=True)
+    message_type_id = Column("message_type_id", Integer, ForeignKey("logs_page_messagetype.id"))
+    message_type = relationship("MessageType", back_populates="sites")
+    site_register = relationship("SiteRegister", back_populates="page")
+
+
+    def __str__(self):  # noqa
+        return str(self.url_site)
+
+
+class SiteRegister(Base):
+    __tablename__ = "logs_page_siteregister"
+
+    id = Column("id", Integer(), primary_key=True)
+    uuid = Column("uuid", UUID(as_uuid=True), default=uuid4)
+    created = Column("created", DateTime(), default=datetime.now())
+    modified = Column("modified", DateTime(), default=datetime.now())
+    message_error = Column("message_error", Text, nullable=False)
+    status_code = Column("status_code", Text, nullable=False)
+    page = relationship("SiteAlert", back_populates="site_register")
+    page_id = Column("page_id", Integer, ForeignKey("logs_page_sitealert.id", ondelete="CASCADE"),
+                  nullable=False)
+
+    def __str__(self):  # noqa
+        return f"{self.status_code} - {self.page}"
