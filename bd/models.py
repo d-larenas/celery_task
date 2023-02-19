e25754e0ff6935c18c25bb84449adebfd314d569
Diego Larenas <111461360+larenas-d@users.noreply.github.com>
2023-02-18 22:12:53 -0300

    :hammer: Modification models

--- bd/models.py
+++ bd/models.py
@@ -7,30 +7,41 @@ from .declarative_base import Base
 from uuid import uuid4
 from celery_service import Setting
 
+from sqlalchemy.ext.hybrid import hybrid_property, hybrid_method
+class ModelBase:
+    """Model base"""
 
-class MessageType(Base):
+    id = Column("id", Integer(), primary_key=True)
+    created = Column("created", DateTime(), default=func.now())
+    modified = Column("modified", DateTime(), onupdate=func.now(), default=func.now())
+    uuid = Column("uuid", UUID(as_uuid=True), default=uuid4)
+
+    @hybrid_property
+    def created_date(self):
+        return self.created.strftime("%d-%m-%Y")
+
+    @hybrid_property
+    def created_time(self):
+        return self.created.strftime("%H:%M:%S")
+
+
+class MessageType(ModelBase, Base):
     """Alert."""
     __tablename__ = "logs_page_messagetype"
 
     id = Column("id", Integer(), primary_key=True)
     email = Column("email", Text, nullable=False)
-    created = Column("created", DateTime(), default=func.now())
-    modified = Column("modified", DateTime(), onupdate=func.now(), default=func.now())
-    uuid = Column("uuid", UUID(as_uuid=True), default=uuid4)
     type_alert = Column("type_alert", Integer(), default=1)
     sites = relationship("SiteAlert", back_populates="message_type")
+
 
     def __str__(self):  # noqa
         return str(self.email)
 
 
-class SiteAlert(Base):
+class SiteAlert(ModelBase, Base):
     __tablename__ = "logs_page_sitealert"
 
-    id = Column("id", Integer(), primary_key=True)
-    uuid = Column("uuid", UUID(as_uuid=True), default=uuid4)
-    created = Column("created", DateTime(), default=func.now())
-    modified = Column("modified", DateTime(), onupdate=func.now(), default=func.now())
     url_site = Column("url_site", Text, nullable=False)
     enable = Column("enable", Boolean, default=True)
     message_type_id = Column("message_type_id", Integer,
@@ -42,14 +53,9 @@ class SiteAlert(Base):
         return str(self.url_site)
 
 
-class SiteRegister(Base):
+class SiteRegister(ModelBase, Base):
     __tablename__ = "logs_page_siteregister"
 
-    id = Column("id", Integer(), primary_key=True)
-    uuid = Column("uuid", UUID(as_uuid=True), default=uuid4)
-    created = Column("created", DateTime(timezone=True), default=func.now())
-    modified = Column("modified", DateTime(timezone=True), onupdate=func.now(),
-                      default=func.now())
     message_error = Column("message_error", Text, nullable=False)
     is_alerted = Column("is_alerted", Boolean, default=False)
     status_code = Column("status_code", Text, nullable=False)
